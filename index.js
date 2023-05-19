4adfdfd27717e1791bd345ea77951d586db30251
elpatron <aidanseantucker@gmail.com>
2023-05-19 12:32:54 -0400

    passed all tests

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+function receivesAFunction(input) {
+    input();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+      
+    }
+}
+
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        // Code for the anonymous function goes here
+    };
+}
+   
+
+
+
+
+
+
